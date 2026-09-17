ff0a58492d6c06c5a219901faae2b9e2a4c29df8
Ezant <116475248+Ezantmood@users.noreply.github.com>
2026-09-17 15:09:17 +0700

    Enable AsyncStorage for Supabase authentication

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -13,6 +13,7 @@ export const supabase = createClient(
     auth: {
       autoRefreshToken: true,
       detectSessionInUrl: false,
+      AsyncStorage: true,
       persistSession: true,
       storage: Platform.OS === 'web' ? undefined : AsyncStorage,
     },
